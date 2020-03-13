8dc0dd225e639fd494b70d04714e217da29f87bb
JONG HWAK LEE <devhwak@gmail.com>
2020-03-13 19:00:57 +0900

    first commit

---
+++ src/main/java/net/hwak/MyHwakApplication.java
@@ -0,0 +1,13 @@
+package net.hwak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHwakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHwakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/hwak/MyHwakApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hwak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHwakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
